3e3b32b9de1493e5f29daf88ee47c17098db267f
tamsil <mohd.tamsil@pw.live>
2023-06-02 13:07:48 +0530

    updated gdrive service

--- services/drive-auth.service.js
+++ services/drive-auth.service.js
@@ -2,8 +2,7 @@ const { google } = require("googleapis");
 const path = require("path");
 
 const getDriveService = () => {
-  console.log(__dirname, '>>>>>>>>>>>>>>')
-  const KEYFILEPATH = path.join(__dirname, "gDriveService.json");
+  const KEYFILEPATH = process.env.NODE_ENV === 'production' ? '/etc/secrets/gDriveService.json' : path.join(__dirname, "gDriveService.json");
   const SCOPES = ["https://www.googleapis.com/auth/drive"];
 
   const auth = new google.auth.GoogleAuth({
