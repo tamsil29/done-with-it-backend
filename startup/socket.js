794e008ce0113d7423ca9930fb8059ecbb21d70c
tamsil <mohd.tamsil@pw.live>
2023-06-10 17:59:10 +0530

    chat socket integration wip

---
+++ startup/socket.js
@@ -0,0 +1,59 @@
+const { Server } = require("socket.io");
+const { Message, Conversation } = require("../models/message");
+const { User } = require("../models/user");
+const {
+  sendPushNotificationOfChat,
+} = require("../utilities/createPushNotification");
+
+module.exports = function (app, httpServer) {
+  const io = new Server(httpServer);
+  // app.set("socketio", io);
+  io.on("connection", (socket) => {
+    console.log("socket connected", socket.id);
+
+    socket.on("join-conversation", (room, cb) => {
+      socket.join(room);
+      cb("success");
+      console.log(`success in joining ${room}`);
+    });
+
+    socket.on("send-message", (message, conversationId, user) => {
+      const newMessage = new Message({
+        conversationId: conversationId,
+        createdAt: Date.now(),
+        message: message,
+        createdBy: JSON.parse(user),
+      });
+      console.log(newMessage);
+      socket.to(conversationId).emit('recieve-message', newMessage)
+      updateConversation(conversationId, newMessage, user)
+    });
+  });
+};
+
+async function updateConversation(conversationId, message, createrUser) {
+  const senderUser = JSON.parse(createrUser)
+  const conversation = await Conversation.findById(conversationId);
+  const targetUserId =
+    conversation.userId1 === senderUser._id
+      ? conversation.userId2
+      : conversation.userId1;
+  const user = await User.findById(targetUserId).select("-password");
+
+  targetUserId === conversation.userId1
+    ? (conversation.user1Data = user)
+    : (conversation.user2Data = user);
+
+  conversation.unreadBy = [targetUserId];
+  conversation.updatedAt = Date.now();
+  conversation.recentMessage = message;
+  await conversation.save();
+  await message.save();
+
+  sendPushNotificationOfChat(
+    user.expoPushToken,
+    senderUser.name,
+    message.message,
+    { data: conversation }
+  );
+}
